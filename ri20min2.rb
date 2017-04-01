4055a54bf60ce4720ae84cca89e1c9e12bae1171
N477f0dd <n477f0dd@gmail.com>
2017-04-01 14:44:55 +0200

    exo ruby

---
+++ ri20min2.rb
@@ -0,0 +1,13 @@
+# Saluer tout le monde
+def say_hi
+  if @names.nil?
+    puts "..."
+  elsif @names.respond_to?("each")
+    # @names est une liste de noms : traitons-les uns par uns
+    @names.each do |name|
+      puts "Hello #{name}!"
+    end
+  else
+    puts "Hello #{@names}!"
+  end
+end
